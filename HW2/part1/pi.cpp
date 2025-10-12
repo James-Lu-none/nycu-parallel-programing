78f9aa44c9f587d318c40bea6ebcf85495932353
James-Lu-none <jamesabcde277@gmail.com>
2025-10-12 19:29:16 +0800

    refactor: simplify Makefile and update pi.cpp

--- HW2/part1/pi.cpp
+++ HW2/part1/pi.cpp
@@ -1,149 +1,132 @@
-#include <cmath>
-#include <cstdio>
-#include <pthread.h>
-#include <stdint.h>
-
-void usage(char *progname) {
-    fprintf(stderr, "Usage: %s <threads> <n>\n", progname);
-    fprintf(stderr, "  <threads> : number of threads to use (positive integer)\n");
-    fprintf(stderr, "  <n>       : number of intervals (positive integer)\n");
-}
-
-long threads;
-long long int n;
-long long int hits;
-pthread_mutex_t mutex;
 #include <immintrin.h>
+#include <random>
+#include <thread>
+#include <vector>
 
-// 256-bit state (4x64)
-typedef struct
+using namespace std;
+
+struct xoshiro256_state
 {
-    uint64_t s[4];
-} xoshiro256_state;
+    __m256i s0, s1, s2, s3;
 
-static inline uint64_t rotl(const uint64_t x, int k)
-{
-    return (x << k) | (x >> (64 - k));
-}
-
-static inline uint64_t xoshiro256ss(xoshiro256_state *state)
-{
-    const uint64_t result = rotl(state->s[1] * 5, 7) * 9;
-
-    const uint64_t t = state->s[1] << 17;
-
-    state->s[2] ^= state->s[0];
-    state->s[3] ^= state->s[1];
-    state->s[1] ^= state->s[2];
-    state->s[0] ^= state->s[3];
-
-    state->s[2] ^= t;
-    state->s[3] = rotl(state->s[3], 45);
-
-    return result;
-}
-
-#include <time.h>
-
-void seed_xoshiro(xoshiro256_state *st, uint64_t seed)
-{
-    // Simple splitmix64 seeding
-    uint64_t z = seed + 0x9E3779B97f4A7C15ULL;
-    for (int i = 0; i < 4; i++)
+    void seed(uint64_t base_seed)
     {
-        z = (z ^ (z >> 30)) * 0xBF58476D1CE4E5B9ULL;
-        z = (z ^ (z >> 27)) * 0x94D049BB133111EBULL;
-        z = z ^ (z >> 31);
-        st->s[i] = z;
+        uint64_t seeds[32];
+        uint64_t z = base_seed;
+        for (int i = 0; i < 32; i++)
+        {
+            z = (z ^ (z >> 30)) * 0xBF58476D1CE4E5B9ULL;
+            z = (z ^ (z >> 27)) * 0x94D049BB133111EBULL;
+            z ^= z >> 31;
+            seeds[i] = z;
+        }
+        s0 = _mm256_loadu_si256((__m256i *)(seeds + 0));
+        s1 = _mm256_loadu_si256((__m256i *)(seeds + 8));
+        s2 = _mm256_loadu_si256((__m256i *)(seeds + 16));
+        s3 = _mm256_loadu_si256((__m256i *)(seeds + 24));
     }
-}
 
-float randf(xoshiro256_state *st)
-{
-    return (xoshiro256ss(st) >> 40) * (1.0f / (1ULL << 24));
-}
+    __m256 randf()
+    {
+        __m256i t = _mm256_slli_epi64(s1, 17);
 
-void *thread_monte_carlo_pi(void *arg)
+        s2 = _mm256_xor_si256(s2, s0);
+        s3 = _mm256_xor_si256(s3, s1);
+        s1 = _mm256_xor_si256(s1, s2);
+        s0 = _mm256_xor_si256(s0, s3);
+        s2 = _mm256_xor_si256(s2, t);
+        s3 = _mm256_or_si256(_mm256_slli_epi64(s3, 45), _mm256_srli_epi64(s3, 64 - 45));
+
+        // Convert s1 (or s0) to float in [0,1)
+        __m256i res = s1; // take s1 as result
+        __m256 resf = _mm256_cvtepi32_ps(_mm256_and_si256(res, _mm256_set1_epi32(0xFFFFFF)));
+        return _mm256_mul_ps(resf, _mm256_set1_ps(1.0f / (1 << 24)));
+    }
+};
+
+struct MonteCarloArgs
 {
-    long threadId = (long)arg;
+    uint64_t start;
+    uint64_t chunk;
+    uint64_t hits;
+};
+
+static void monte_carlo_thread(MonteCarloArgs *args)
+{
     xoshiro256_state rng;
-    seed_xoshiro(&rng, time(NULL) ^ (threadId * 0x9E3779B1));
-    int baseChunk = n / threads;
-    int remainder = n % threads;
-    int start = threadId * baseChunk + (threadId < remainder ? threadId : remainder);
-    int end = start + baseChunk + (threadId < remainder ? 1 : 0);
-    long local_hits = 0;
-    
-    int simd_iters = (end - start) / 8;
-    int leftover = (end - start) % 8;
+    rng.seed((uint64_t)random_device{}() + args->start);
 
-    for (int i = 0; i < simd_iters; ++i) {
-        float xs[8], ys[8];
-        for (int j = 0; j < 8; ++j) {
-            xs[j] = randf(&rng);
-            ys[j] = randf(&rng);
-        }
-        __m256 vx = _mm256_loadu_ps(xs);
-        __m256 vy = _mm256_loadu_ps(ys);
-        __m256 vxsq = _mm256_mul_ps(vx, vx);
-        __m256 vysq = _mm256_mul_ps(vy, vy);
-        __m256 vsum = _mm256_add_ps(vxsq, vysq);
-        __m256 vmask = _mm256_cmp_ps(vsum, _mm256_set1_ps(1.0f), _CMP_LE_OS);
-        local_hits += _mm_popcnt_u32(_mm256_movemask_ps(vmask));
+    uint64_t local_hits = 0;
+    uint64_t chunk = args->chunk;
+
+    __m256 one = _mm256_set1_ps(1.0f);
+
+    for (uint64_t i = 0; i < chunk / 8; i++)
+    {
+        __m256 x = rng.randf();
+        __m256 y = rng.randf();
+        __m256 sum = _mm256_add_ps(_mm256_mul_ps(x, x), _mm256_mul_ps(y, y));
+        __m256 mask = _mm256_cmp_ps(sum, one, _CMP_LE_OS);
+        local_hits += _mm_popcnt_u32(_mm256_movemask_ps(mask));
     }
 
-    for (int i = 0; i < leftover; ++i) {
-        float x = randf(&rng);
-        float y = randf(&rng);
-        if (x * x + y * y <= 1.0f) {
-            local_hits++;
-        }
-    }
+    args->hits = local_hits;
+}
 
-    pthread_mutex_lock(&mutex);
-    hits += local_hits;
-    pthread_mutex_unlock(&mutex);
-    return NULL;
+void usage(char *progname)
+{
+    fprintf(stderr, "Usage: %s <threads> <n>\n", progname);
+    fprintf(stderr, " <threads> : number of threads to use (positive integer)\n");
+    fprintf(stderr, " <n> : number of intervals (positive integer)\n");
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 3) {
+    if (argc < 3)
+    {
         usage(argv[0]);
         return 1;
     }
 
+    uint8_t num_threads;
+    uint64_t n;
     char *endptr;
-    threads = strtol(argv[1], &endptr, 10);
-    if (*endptr != '\0' || threads <= 0) {
+    num_threads = strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0' || num_threads <= 0)
+    {
         fprintf(stderr, "Invalid value for threads: %s\n", argv[1]);
         usage(argv[0]);
         return 1;
     }
-
     n = strtoll(argv[2], &endptr, 10);
-    if (*endptr != '\0') {
+    if (*endptr != '\0' || n <= 0)
+    {
         fprintf(stderr, "Invalid value for n: %s\n", argv[2]);
         usage(argv[0]);
         return 1;
     }
 
-    hits = 0;
-    pthread_mutex_init(&mutex, NULL);
+    vector<thread> threads(num_threads);
+    vector<MonteCarloArgs> threadArgs(num_threads);
 
-    pthread_t *thread_handles;
-    thread_handles = (pthread_t *)malloc(threads * sizeof(pthread_t));
+    uint64_t chunk = n / num_threads;
+    uint64_t start = 0;
 
-    for (long t=0; t<threads; ++t){
-        pthread_create(&thread_handles[t], NULL, thread_monte_carlo_pi, (void*)t);
-    }
-    for (long t = 0; t < threads; ++t)
+    for (uint8_t t = 0; t < num_threads; ++t)
     {
-        pthread_join(thread_handles[t], NULL);
+        threadArgs[t] = {start, chunk, 0};
+        start += chunk;
+        threads[t] = thread(monte_carlo_thread, &threadArgs[t]);
     }
 
-    double pi = hits * 4.0 / n;
-    printf("%lf\n", pi);
-    free(thread_handles);
+    uint64_t total_hits = 0;
+    for (uint8_t t = 0; t < num_threads; ++t)
+    {
+        threads[t].join();
+        total_hits += threadArgs[t].hits;
+    }
+
+    float pi = 4.0 * total_hits / n;
+    printf("%f\n", pi);
     return 0;
 }
