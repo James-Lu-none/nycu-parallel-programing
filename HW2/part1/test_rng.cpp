626aa6361c062585b294db1d34f39a90754a5468
James-Lu-none <jamesabcde277@gmail.com>
2025-10-12 21:01:00 +0800

    update test_rng.cpp

--- HW2/part1/test_rng.cpp
+++ HW2/part1/test_rng.cpp
@@ -1,84 +1,83 @@
+#include <cmath>
 #include <immintrin.h>
+#include <numeric>
 #include <random>
 #include <thread>
 #include <vector>
-#include <chrono>
-#include <iostream>
 
 #include "rng.hpp"
 
 using namespace std;
+constexpr double PI = 3.14159265358979323846;
 
-#include <chrono>
-#include <immintrin.h>
-#include <iostream>
-#include <random>
-#include <thread>
-#include <vector>
-using namespace std;
+template <typename RNG> double monte_carlo_pi_single(uint64_t samples, uint64_t seed)
+{
+    RNG rng;
+    rng.seed(seed);
+    uint64_t hits = 0;
+    float buffer[8];
+    float buffer2[8];
+    uint64_t simd_iters = samples / 8;
 
-template <typename RNG> void benchmark_rng(const string &name, int num_samples, int num_threads)
+    for (uint64_t i = 0; i < simd_iters; i++)
+    {
+        _mm256_storeu_ps(buffer, rng.randf());
+        _mm256_storeu_ps(buffer2, rng.randf());
+        for (uint64_t j = 0; j < 8; j++)
+        {
+            float x = buffer[j];
+            float y = buffer2[j];
+            if (x * x + y * y <= 1.0f)
+                hits++;
+        }
+    }
+    return hits;
+}
+
+template <typename RNG> double monte_carlo_pi(uint64_t total_samples, uint64_t num_threads)
 {
     vector<thread> threads(num_threads);
     vector<double> results(num_threads, 0.0);
 
-    auto start_time = chrono::high_resolution_clock::now();
-
-    for (int t = 0; t < num_threads; t++)
+    for (uint64_t t = 0; t < num_threads; t++)
     {
         threads[t] = thread(
-            [t, &results, num_samples, num_threads]()
-            {
-                RNG rng;
-                rng.seed((uint64_t)random_device{}() + t);
-
-                double local_sum = 0.0;
-                int samples_per_thread = num_samples / num_threads;
-                int simd_iters = samples_per_thread / 8;
-                int leftover = samples_per_thread % 8;
-                float buffer[8];
-
-                for (int i = 0; i < simd_iters; i++)
-                {
-                    __m256 r = rng.randf();
-                    _mm256_storeu_ps(buffer, r);
-                    for (int j = 0; j < 8; j++)
-                        local_sum += buffer[j];
-                }
-
-                for (int i = 0; i < leftover; i++)
-                {
-                    __m256 r = rng.randf();
-                    _mm256_storeu_ps(buffer, r);
-                    local_sum += buffer[0]; // use first value for leftover
-                }
-
-                results[t] = local_sum;
+            [t, &results, total_samples, num_threads]() {
+                double pi_est = monte_carlo_pi_single<RNG>(total_samples / num_threads, (uint64_t)random_device{}() + t);
+                results[t] = pi_est;
             });
     }
 
     for (auto &th : threads)
         th.join();
+    
+    double sum = accumulate(results.begin(), results.end(), 0.0);
+    return (4.0 * sum) / total_samples;
+}
 
-    double total_sum = 0.0;
-    for (const auto &res : results)
-        total_sum += res;
+// Statistical test
+template <typename RNG>
+void monte_carlo_pi_stat(uint64_t total_samples, uint64_t num_threads, const string &name)
+{
+    using namespace std::chrono;
 
-    auto end_time = chrono::high_resolution_clock::now();
-    chrono::duration<double> duration = end_time - start_time;
+    auto start = high_resolution_clock::now();
 
-    cout << name << ": Sum = " << total_sum << ", Time = " << duration.count() << " seconds"
-         << endl;
+    double estimate = monte_carlo_pi<RNG>(total_samples, num_threads);
+
+    auto end = high_resolution_clock::now();
+    double elapsed_sec = duration<double>(end - start).count();
+
+    printf("%s: pi = %f, Error = %f Time = %.6f sec\n", name.c_str(), estimate, estimate - PI, elapsed_sec);
 }
 
 int main()
 {
-    const int num_samples = 100'000'000;
-    const int num_threads = 1;
+    const uint64_t num_samples = 5000000000;
+    const uint64_t num_threads = 8;
 
-    benchmark_rng<xoshiro256_state>("xoshiro256", num_samples, num_threads);
-    benchmark_rng<xoshiro64_state>("xoshiro64", num_samples, num_threads);
-    benchmark_rng<xorshift64_state>("xorshift64", num_samples, num_threads);
-
+    monte_carlo_pi_stat<xoshiro256_state>(num_samples, num_threads, "xoshiro256");
+    monte_carlo_pi_stat<xoshiro64_state>(num_samples, num_threads, "xoshiro64");
+    monte_carlo_pi_stat<xorshift64_state>(num_samples, num_threads, "xorshift64");
     return 0;
 }
