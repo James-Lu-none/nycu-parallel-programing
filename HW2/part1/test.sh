2c3b70e039a7df6b97e34c35da090280b07d285a
James-Lu-none <jamesabcde277@gmail.com>
2025-10-13 20:09:56 +0800

    update test script to include test count in failure message

--- HW2/part1/test.sh
+++ HW2/part1/test.sh
@@ -3,15 +3,17 @@
 command="./pi.out 4 100000000"
 
 # run command until its 5th output character is not 1
-
+n=0
 while true
 do
     output=$($command)
+    n=$((n+1))
     echo "Output: $output"
+
     first_char=${output:0:1}
     fifth_char=${output:4:1}
     if [ "$fifth_char" != "1" ]; then
-        echo "Test failed."
+        echo "Test $n failed."
         exit 1
     fi
 done
