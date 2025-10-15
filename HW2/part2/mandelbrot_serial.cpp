4ca0876d0716e3f2f9b17de30e1e5091a8b794ef
James-Lu-none <jamesabcde277@gmail.com>
2025-10-15 17:00:54 +0800

    feat: implement SIMD vectorization for Mandelbrot serial

--- HW2/part2/mandelbrot_serial.cpp
+++ HW2/part2/mandelbrot_serial.cpp
@@ -36,6 +36,7 @@
    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+#include <immintrin.h>
 
 namespace
 {
@@ -95,18 +96,82 @@ void mandelbrot_serial(float x0,
 {
     float dx = (x1 - x0) / (float)width;
     float dy = (y1 - y0) / (float)height;
+    __m128 _1 = _mm_set1_ps(1.f);
+    __m128 _2 = _mm_set1_ps(2.f);
+    __m128 _4 = _mm_set1_ps(4.f);
+    __m128 _025 = _mm_set1_ps(0.25f);
+    __m128 _00625 = _mm_set1_ps(0.0625f);
 
     int end_row = start_row + total_rows;
 
     for (int j = start_row; j < end_row; j++)
     {
-        for (int i = 0; i < width; ++i)
+        __m128 y_vec = _mm_set1_ps(y0 + j * dy);
+        for (int i = 0; i < width; i+=4)
         {
-            float x = x0 + ((float)i * dx);
-            float y = y0 + ((float)j * dy);
+            float x_vals[4];
+            for (int k = 0; k < 4; ++k)
+                x_vals[k] = x0 + (i + k) * dx;
+            __m128 x_vec = _mm_loadu_ps(x_vals);
 
-            int index = ((j * width) + i);
-            output[index] = mandel(x, y, max_iterations);
+            // check if c=x+yi is in the main cardioid: q*(q+(x-0.25)) <= 0.25*y^2,
+            // q=(x-0.25f)^2+y^2
+            __m128 x025 = _mm_sub_ps(x_vec, _025);
+            __m128 y2 = _mm_mul_ps(y_vec, y_vec);
+            __m128 q = _mm_add_ps(_mm_mul_ps(x025, x025), y2);
+            __m128 left = _mm_mul_ps(q, _mm_add_ps(q, x025));
+            __m128 right = _mm_mul_ps(_025, y2);
+            __m128 mask_cardioid = _mm_cmple_ps(left, right);
+
+            // check if c=x+yi is in the period-2 bulb (main disk): (x+1)^2+y^2 <= 0.0625
+            __m128 xa1 = _mm_add_ps(x_vec, _1);
+            __m128 mask_disk = _mm_cmple_ps(_mm_add_ps(_mm_mul_ps(xa1, xa1), y2), _00625);
+
+            __m128 mask_inside = _mm_or_ps(mask_cardioid, mask_disk);
+            int inside_mask = _mm_movemask_ps(mask_inside);
+
+            __m128i iter = _mm_setzero_si128();
+
+            // printf("row %d, col %d, inside_mask %d\n", row, i, inside_mask);
+            if (inside_mask == 4)
+            {
+                iter = _mm_set1_epi32(256);
+            }
+            else
+            {
+                __m128 z_re = x_vec, c_re = x_vec;
+                __m128 z_im = y_vec, c_im = y_vec;
+                for (int n = 0; n < 256; ++n)
+                {
+                    __m128 z_re2 = _mm_mul_ps(z_re, z_re);
+                    __m128 z_im2 = _mm_mul_ps(z_im, z_im);
+                    __m128 mag2 = _mm_add_ps(z_re2, z_im2);
+
+                    __m128 mask = _mm_cmple_ps(mag2, _4);
+                    if (_mm_movemask_ps(mask) == 0)
+                        break;
+
+                    __m128 new_re = _mm_sub_ps(z_re2, z_im2);
+                    __m128 new_im = _mm_mul_ps(_mm_mul_ps(z_re, z_im), _2);
+
+                    z_re = _mm_add_ps(c_re, new_re);
+                    z_im = _mm_add_ps(c_im, new_im);
+
+                    __m128i mask_i = _mm_castps_si128(mask);
+                    iter = _mm_add_epi32(iter, _mm_and_si128(mask_i, _mm_set1_epi32(1)));
+                }
+            }
+
+            int iter_vals[4];
+            _mm_storeu_si128((__m128i *)iter_vals, iter);
+
+            for (int k = 0; k < 4; ++k)
+            {
+                if (inside_mask & (1 << k))
+                    output[j * width + i + k] = 256;
+                else
+                    output[j * width + i + k] = iter_vals[k];
+            }
         }
     }
 }
