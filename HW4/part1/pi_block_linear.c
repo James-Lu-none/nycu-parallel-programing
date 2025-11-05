4bac64bb7eb633a9f2efeb34ec48f2d9d6c7d28e
James-Lu-none <jamesabcde277@gmail.com>
2025-11-05 14:45:38 +0800

    finish todos in pi_block_linear.c

--- HW4/part1/pi_block_linear.c
+++ HW4/part1/pi_block_linear.c
@@ -16,19 +16,44 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: init MPI
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
+    long long int local_tosses = tosses / world_size;
+    unsigned int seed = time(NULL) * world_rank;
+    double x, y;
+    int count = 0;
+    for (long long int i = 0; i < local_tosses; i++)
+    {
+        x = rand_r(&seed) / (double)RAND_MAX;
+        y = rand_r(&seed) / (double)RAND_MAX;
+        if (x * x + y * y <= 1.0)
+        {
+            count++;
+        }
+    }
+    double local_pi = 4.0 * count / local_tosses;
     if (world_rank > 0)
     {
         // TODO: handle workers
+        MPI_Send(&local_pi, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
     else if (world_rank == 0)
     {
         // TODO: main
+        pi_result = local_pi;
+        for (int i = 1; i < world_size; i++)
+        {
+            double local_pi;
+            MPI_Recv(&local_pi, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            pi_result += local_pi;    
+        }
     }
 
     if (world_rank == 0)
     {
         // TODO: process PI result
+        pi_result /= world_size;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
