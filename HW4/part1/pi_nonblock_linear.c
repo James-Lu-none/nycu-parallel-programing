9a789188ed819ef60c4c660e84c44e582adbad1f
James-Lu-none <jamesabcde277@gmail.com>
2025-11-05 15:45:21 +0800

    implement todos in pi_nonblock_linear.c

--- HW4/part1/pi_nonblock_linear.c
+++ HW4/part1/pi_nonblock_linear.c
@@ -16,24 +16,49 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    long long int local_tosses = tosses / world_size;
+    unsigned int seed = 314581029 * world_rank;
+    double x, y;
+    int count = 0;
+    for (long long int i = 0; i < local_tosses; i++)
+    {
+        x = rand_r(&seed) / (double)RAND_MAX;
+        y = rand_r(&seed) / (double)RAND_MAX;
+        if (x * x + y * y <= 1.0)
+        {
+            count++;
+        }
+    }
+    double local_pi = 4.0 * count / local_tosses;
 
     if (world_rank > 0)
     {
-        // TODO: MPI workers
+        MPI_Send(&local_pi, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
-    else if (world_rank == 0)
+    else
     {
-        // TODO: non-blocking MPI communication.
-        // Use MPI_Irecv, MPI_Wait or MPI_Waitall.
+        double *recv_pis = malloc(sizeof(double) * world_size);
+        MPI_Request *requests = malloc(sizeof(MPI_Request) * world_size);
+        
+        for (int i = 1; i < world_size; i++)
+        {
+            MPI_Irecv(&recv_pis[i], 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &requests[i]);
+        }
 
-        // MPI_Request requests[];
+        MPI_Waitall(world_size - 1, &requests[1], MPI_STATUSES_IGNORE);
 
-        // MPI_Waitall();
-    }
+        for (int i = 1; i < world_size; i++)
+        {
+            local_pi += recv_pis[i];
+        }
 
-    if (world_rank == 0)
-    {
-        // TODO: PI result
+        pi_result = local_pi / world_size;
+
+        free(recv_pis);
+        free(requests);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
