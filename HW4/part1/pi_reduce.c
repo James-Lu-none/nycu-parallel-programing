69b951db3c8567fb6ead2704ae02b5eb5a4ed650
James-Lu-none <jamesabcde277@gmail.com>
2025-11-05 16:46:39 +0800

    implement todos in pi_reduce.c

--- HW4/part1/pi_reduce.c
+++ HW4/part1/pi_reduce.c
@@ -16,12 +16,30 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    long long int local_tosses = tosses / world_size;
+    unsigned int seed = 314581029 * world_rank;
+    double x, y;
+    int count = 0;
+    for (long long int i = 0; i < local_tosses; i++)
+    {
+        x = rand_r(&seed) / (double)RAND_MAX;
+        y = rand_r(&seed) / (double)RAND_MAX;
+        if (x * x + y * y <= 1.0)
+        {
+            count++;
+        }
+    }
+    double local_pi = 4.0 * count / local_tosses;
 
     // TODO: use MPI_Reduce
-
+    MPI_Reduce(&local_pi, &pi_result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     if (world_rank == 0)
     {
         // TODO: PI result
+        pi_result /= world_size;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
