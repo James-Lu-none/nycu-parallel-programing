38a076f3c3608b2b60ae637dea1ab086310f5921
James-Lu-none <jamesabcde277@gmail.com>
2025-11-06 14:08:19 +0800

    refactor: move all broadcasting work to matrix construction

--- HW4/part2/matmul.cc
+++ HW4/part2/matmul.cc
@@ -6,6 +6,19 @@
 #include <unistd.h>
 #include <string.h>
 
+int *rows;
+int *numbers_A;
+int *offsets_A;
+int *numbers_C;
+int *offsets_C;
+
+int *local_A;
+int *local_BT;
+int *local_C;
+
+int local_rows;
+
+int world_rank, world_size;
 
 void construct_matrices(
     int n, int m, int l, const int *a_mat, const int *b_mat, int **a_mat_ptr, int **b_mat_ptr)
@@ -18,48 +31,23 @@ void construct_matrices(
      *
      * The matrix multiplication will be performed on a_mat_ptr and b_mat_ptr.
      */
-    int world_rank, world_size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    if (world_rank != 0)
-    {
-        // Non-root ranks can't construct matrices since *a_mat, *b_mat are only available in rank 0
-        *a_mat_ptr = nullptr;
-        *b_mat_ptr = nullptr;
-        return;
-    }
-    int *A = new int[n * m];
-    memcpy(A, a_mat, n * m * sizeof(int));
+    // dont use them at all
+    *a_mat_ptr = nullptr;
+    *b_mat_ptr = nullptr;
 
-    // there is a trap here, in main.cc b_mat is loaded incorrectly as a m x l matrix, which is already transposed
-    int *BT = new int[l * m];
-    memcpy(BT, b_mat, m * l * sizeof(int));
-
-    *a_mat_ptr = A;
-    *b_mat_ptr = BT;
-}
-
-void matrix_multiply(
-    const int n, const int m, const int l, const int *a_mat, const int *bt_mat, int *out_mat)
-{
-    /* TODO: Perform matrix multiplication on a_mat and b_mat. Which are the matrices you've
-     * constructed. The result should be stored in out_mat, which is a continuous memory placing n *
-     * l elements of int. You need to make sure rank 0 receives the result.
-     */
-    int world_rank, world_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     int base = n / world_size;
     int rem = n % world_size;
 
-    // numbers and offsets of A and C for each process
-    int *rows = new int[world_size];
-    int *numbers_A = new int[world_size];
-    int *offsets_A = new int[world_size];
-    int *numbers_C = new int[world_size];
-    int *offsets_C = new int[world_size];
+    // calculate numbers and offsets of A and C for each process
+    rows = new int[world_size];
+    numbers_A = new int[world_size];
+    offsets_A = new int[world_size];
+    numbers_C = new int[world_size];
+    offsets_C = new int[world_size];
 
     int offset_A_tmp = 0, offset_C_tmp = 0;
     for (int r = 0; r < world_size; ++r)
@@ -75,26 +63,35 @@ void matrix_multiply(
         offset_C_tmp += numbers_C[r];
     }
 
-    const int local_rows = rows[world_rank];
+    local_rows = rows[world_rank];
 
-    int *local_BT = NULL;
     // Allocate local_BT for non-root ranks since *bt_mat is const and cannot be used directly
     if (world_rank == 0)
     {
-        local_BT = (int *)bt_mat;
+        // there is a trap here, in main.cc b_mat is loaded incorrectly as a m x l matrix, which is already transposed
+        local_BT = (int *)b_mat;
     }
     else
     {
         local_BT = new int[l * m];
     }
 
-    int *local_A = new int[local_rows * m];
-    int *local_C = new int[local_rows * l];
+    local_A = new int[local_rows * m];
+    local_C = new int[local_rows * l];
 
     // send A's rows and broadcast B^T to all processes since they dont have them
     MPI_Scatterv(a_mat, numbers_A, offsets_A, MPI_INT, local_A, numbers_A[world_rank], MPI_INT, 0, MPI_COMM_WORLD);
     // broadcast BT since all processes need entire B table regardless the number of rows assigned
     MPI_Bcast(local_BT, l * m, MPI_INT, 0, MPI_COMM_WORLD);
+}
+
+void matrix_multiply(
+    const int n, const int m, const int l, const int *a_mat, const int *bt_mat, int *out_mat)
+{
+    /* TODO: Perform matrix multiplication on a_mat and b_mat. Which are the matrices you've
+     * constructed. The result should be stored in out_mat, which is a continuous memory placing n *
+     * l elements of int. You need to make sure rank 0 receives the result.
+     */
 
     for (int i = 0; i < local_rows; ++i)
     {
@@ -109,8 +106,14 @@ void matrix_multiply(
         }
     }
 
-    MPI_Gatherv(local_C, numbers_C[world_rank], MPI_INT, out_mat, numbers_C, offsets_C, MPI_INT, 0,
-                MPI_COMM_WORLD);
+    MPI_Gatherv(local_C, numbers_C[world_rank], MPI_INT, out_mat, numbers_C, offsets_C, MPI_INT, 0, MPI_COMM_WORLD);
+}
+
+void destruct_matrices(int *a_mat, int *b_mat)
+{
+    /* TODO */
+    delete[] a_mat;
+    delete[] b_mat;
 
     delete[] local_A;
     delete[] local_C;
@@ -123,11 +126,4 @@ void matrix_multiply(
     {
         delete[] local_BT;
     }
-}
-
-void destruct_matrices(int *a_mat, int *b_mat)
-{
-    /* TODO */
-    delete[] a_mat;
-    delete[] b_mat;
 }
