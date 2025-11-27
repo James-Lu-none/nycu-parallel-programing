9bf29da867d7b24563aa6437beeecd885a18ef16
James-Lu-none <jamesabcde277@gmail.com>
2025-11-27 19:03:58 +0800

    add kernel3 implementation with GROUP_SIZE

--- HW5/kernel3.cu
+++ HW5/kernel3.cu
@@ -1,16 +1,61 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cuda.h>
+#include <cuda_runtime.h>
+#include <cstring>
 
 // Each CUDA thread processes a group of pixels. Use cudaHostAlloc to allocate the host memory, and
 // use cudaMallocPitch to allocate GPU memory. You can try different size of the group. Name the
 // file kernel3.cu.
-__global__ void mandel_kernel()
+__device__ int mandel(float c_re, float c_im, int max_iterations)
 {
-    // To avoid error caused by the floating number, use the following pseudo code
-    //
-    // float x = lowerX + thisX * stepX;
-    // float y = lowerY + thisY * stepY;
+    float z_re = c_re;
+    float z_im = c_im;
+    int i = 0;
+    for (; i < max_iterations; ++i)
+    {
+        if (z_re * z_re + z_im * z_im > 4.f)
+            break;
+        float new_re = (z_re * z_re) - (z_im * z_im);
+        float new_im = 2.f * z_re * z_im;
+        z_re = c_re + new_re;
+        z_im = c_im + new_im;
+    }
+    return i;
+}
+
+// let each thread process multiple adjacent pixels along X for better throughput
+template <int GROUP_SIZE>
+__global__ void mandel_kernel(float lower_x,
+                              float lower_y,
+                              float step_x,
+                              float step_y,
+                              int max_iterations,
+                              int res_x,
+                              int res_y,
+                              size_t pitch,
+                              int *output)
+{
+    int tx = blockIdx.x * blockDim.x + threadIdx.x;
+    int this_y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (this_y >= res_y)
+        return;
+
+    int base_x = tx * GROUP_SIZE;
+    if (base_x >= res_x)
+        return;
+
+    int *row = (int *)((char *)output + this_y * pitch);
+#pragma unroll
+    for (int i = 0; i < GROUP_SIZE; ++i)
+    {
+        int this_x = base_x + i;
+        if (this_x >= res_x)
+            break;
+        float x = lower_x + this_x * step_x;
+        float y = lower_y + this_y * step_y;
+        row[this_x] = mandel(x, y, max_iterations);
+    }
 }
 
 // Host front-end function that allocates the memory and launches the GPU kernel
@@ -25,4 +70,27 @@ void host_fe(float upper_x,
 {
     float step_x = (upper_x - lower_x) / (float)res_x;
     float step_y = (upper_y - lower_y) / (float)res_y;
+
+    int img_size = res_x * res_y * sizeof(int);
+    int *host_img = nullptr;
+    cudaHostAlloc((void **)&host_img, img_size, cudaHostAllocDefault);
+
+    int *device_img = nullptr;
+    size_t pitch = 0;
+    cudaMallocPitch((void **)&device_img, &pitch, res_x * sizeof(int), res_y);
+
+    constexpr int group_size = 4;
+    dim3 block(32, 8, 1);
+    dim3 grid((res_x + group_size * block.x - 1) / (group_size * block.x),
+              (res_y + block.y - 1) / block.y);
+    mandel_kernel<group_size>
+        <<<grid, block>>>(lower_x, lower_y, step_x, step_y, max_iterations, res_x, res_y, pitch,
+                          device_img);
+
+    cudaMemcpy2D(host_img, res_x * sizeof(int), device_img, pitch, res_x * sizeof(int), res_y,
+                 cudaMemcpyDeviceToHost);
+    std::memcpy(img, host_img, img_size);
+
+    cudaFree(device_img);
+    cudaFreeHost(host_img);
 }
