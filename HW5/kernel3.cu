1972aa1c39685057c8dfc53342b26dea22389b81
James-Lu-none <jamesabcde277@gmail.com>
2025-11-27 13:09:47 +0800

    add templates for CUDA kernel implementation

---
+++ HW5/kernel3.cu
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cuda.h>
+
+// Each CUDA thread processes a group of pixels. Use cudaHostAlloc to allocate the host memory, and
+// use cudaMallocPitch to allocate GPU memory. You can try different size of the group. Name the
+// file kernel3.cu.
+__global__ void mandel_kernel()
+{
+    // To avoid error caused by the floating number, use the following pseudo code
+    //
+    // float x = lowerX + thisX * stepX;
+    // float y = lowerY + thisY * stepY;
+}
+
+// Host front-end function that allocates the memory and launches the GPU kernel
+void host_fe(float upper_x,
+             float upper_y,
+             float lower_x,
+             float lower_y,
+             int *img,
+             int res_x,
+             int res_y,
+             int max_iterations)
+{
+    float step_x = (upper_x - lower_x) / (float)res_x;
+    float step_y = (upper_y - lower_y) / (float)res_y;
+}
---
+++ HW5/kernel4.cu
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cuda.h>
+
+// compete with the reference time. The competition is judged by the running times of your program
+// for generating both views for max_iterations of 100000 with the metric below.
+__global__ void mandel_kernel()
+{
+    // To avoid error caused by the floating number, use the following pseudo code
+    //
+    // float x = lowerX + thisX * stepX;
+    // float y = lowerY + thisY * stepY;
+}
+
+// Host front-end function that allocates the memory and launches the GPU kernel
+void host_fe(float upper_x,
+             float upper_y,
+             float lower_x,
+             float lower_y,
+             int *img,
+             int res_x,
+             int res_y,
+             int max_iterations)
+{
+    float step_x = (upper_x - lower_x) / (float)res_x;
+    float step_y = (upper_y - lower_y) / (float)res_y;
+}
